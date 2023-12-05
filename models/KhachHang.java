ce27d8cf1270b82df3fde03ed2920618d1fe3e2c
doggu <trggiathanh2003@gmail.com>
2023-12-05 11:56:59 +0700

    bug tumlum

--- models/KhachHang.java
+++ models/KhachHang.java
@@ -13,7 +13,7 @@ public class KhachHang extends ConNguoi {
 
   public KhachHang(String name, String gender, String address, String email, String sdt, int age, String customerId,
       String kindOfCustomer) {
-    super(name, age, address, email, sdt, gender);
+    super(name, gender, address, email, sdt, age);
     this.customerId = customerId;
     this.kindOfCustomer = kindOfCustomer;
   }
