b48db406d8a5b5ccb998512dd11fbe8ff1edf206
Ndnkhuong <142616977+Ndnkhuong@users.noreply.github.com>
2023-12-11 13:23:17 +0700

    thongke

--- controllers/ThongKeKinhDoanh.java
+++ controllers/ThongKeKinhDoanh.java
@@ -3,6 +3,7 @@ package controllers;
 import java.io.FileNotFoundException;
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 import models.ThongKe;
@@ -36,7 +37,8 @@ public class ThongKeKinhDoanh implements ControllerInterface {
         TK = new ThongKe[result.length];
         for (int i = 0; i < result.length; i++) {
             String[] row = result[i].split(";");
-            TK[i] = new ThongKe(row[0], row[1],Integer.parseInt(row[2]), Integer.parseInt(row[3]), LocalDate.parse(row[4]),
+            TK[i] = new ThongKe(row[0], row[1], Integer.parseInt(row[2]), Integer.parseInt(row[3]),
+                    LocalDate.parse(row[4]),
                     Integer.parseInt(row[5]));
         }
         return TK;
@@ -59,6 +61,16 @@ public class ThongKeKinhDoanh implements ControllerInterface {
     public void Delete() {
     }
 
+    public int calculateTotalQuantityForCustomer(String customerId) {
+        int totalQuantity = 0;
+        for (ThongKe thongKe : TK) {
+            if (thongKe.getCustomerId().equals(customerId)) {
+                totalQuantity += thongKe.getAmount();
+            }
+        }
+        return totalQuantity;
+    }
+
     public ThongKe[] addSanPham(ThongKe[] TK, ThongKe thongKe) {
         TK = Arrays.copyOf(TK, TK.length + 1);
         TK[TK.length - 1] = thongKe;
@@ -77,6 +89,7 @@ public class ThongKeKinhDoanh implements ControllerInterface {
         System.out.println("\t\t\t\t\t\t\t\t +-----------------------------------------+");
         System.out.println("\t\t\t\t\t\t\t\t |1. ID sản phẩm                           |");
         System.out.println("\t\t\t\t\t\t\t\t |2. Thống kê theo khoảng thời gian        |");
+        System.out.println("\t\t\t\t\t\t\t\t |3. Khách hàng mua nhiều                  |");
         System.out.println("\t\t\t\t\t\t\t\t +-----------------------------------------+");
         System.out.print("\t\t\t\t\t\t\t\t - Mời Bạn Nhập Lựa Chọn: ");
         String choose = input.nextLine();
@@ -93,6 +106,27 @@ public class ThongKeKinhDoanh implements ControllerInterface {
                         result = addSanPham(result, thongKe);
                     }
                 }
+                System.out.println("Danh sách thống kê:");
+                String header = String.format("| %-8s | %-12s | %-10s | %-12s | %-15s | %-15s |", "ID khách",
+                        "ID sản phẩm",
+                        "Số lượng", "Giá",
+                        "Ngày", "Tổng");
+                System.out.format(
+                        "+----------+--------------+------------+--------------+-----------------+-----------------+%n");
+                System.out.println(header);
+                System.out.format(
+                        "+----------+--------------+------------+--------------+-----------------+-----------------+%n");
+                for (ThongKe thongKe : result) {
+                    String read = String.format("| %-8s | %-12s | %-10s | %-12s | %-15s | %-15s |",
+                            thongKe.getCustomerId(),
+                            thongKe.getProductId(),
+                            thongKe.getAmount(), thongKe.getPrice(), thongKe.getDate(), thongKe.getTotalAmount());
+                    System.out.println(read);
+                }
+                System.out.format(
+                        "+----------+--------------+------------+--------------+-----------------+-----------------+%n");
+                System.out.println("Tổng số lượng đã bán ra: " + totalAmount);
+                System.out.println("Doanh thu: " + totalPrice);
             }
             case "2" -> {
                 LocalDate startDate = inputDate("Nhập ngày bắt đầu (yyyy-MM-dd): ");
@@ -105,30 +139,64 @@ public class ThongKeKinhDoanh implements ControllerInterface {
                         result = addSanPham(result, thongKe);
                     }
                 }
+                System.out.println("Danh sách thống kê:");
+                String header = String.format("| %-8s | %-12s | %-10s | %-12s | %-15s | %-15s |", "ID khách",
+                        "ID sản phẩm",
+                        "Số lượng", "Giá",
+                        "Ngày", "Tổng");
+                System.out.format(
+                        "+----------+--------------+------------+--------------+-----------------+-----------------+%n");
+                System.out.println(header);
+                System.out.format(
+                        "+----------+--------------+------------+--------------+-----------------+-----------------+%n");
+                for (ThongKe thongKe : result) {
+                    String read = String.format("| %-8s | %-12s | %-10s | %-12s | %-15s | %-15s |",
+                            thongKe.getCustomerId(),
+                            thongKe.getProductId(),
+                            thongKe.getAmount(), thongKe.getPrice(), thongKe.getDate(), thongKe.getTotalAmount());
+                    System.out.println(read);
+                }
+                System.out.format(
+                        "+----------+--------------+------------+--------------+-----------------+-----------------+%n");
+                System.out.println("Tổng số lượng đã bán ra: " + totalAmount);
+                System.out.println("Doanh thu: " + totalPrice);
             }
-
+            case "3" -> {
+                // Sắp xếp danh sách theo tổng số lượng mua giảm dần
+                Arrays.sort(TK,
+                        Comparator.comparingInt(
+                                thongKe -> calculateTotalQuantityForCustomer(((ThongKe) thongKe).getCustomerId()))
+                                .reversed());
+                System.out.println("Danh sách thống kê:");
+                String header = String.format("| %-8s | %-12s | %-10s | %-12s | %-15s | %-15s |", "ID khách",
+                        "ID sản phẩm",
+                        "Số lượng", "Giá",
+                        "Ngày", "Tổng");
+                System.out.format(
+                        "+----------+--------------+------------+--------------+-----------------+-----------------+%n");
+                System.out.println(header);
+                System.out.format(
+                        "+----------+--------------+------------+--------------+-----------------+-----------------+%n");
+                for (ThongKe thongKe : TK) {
+                    String read = String.format("| %-8s | %-12s | %-10s | %-12s | %-15s | %-15s |",
+                            thongKe.getCustomerId(),
+                            thongKe.getProductId(), thongKe.getAmount(), thongKe.getPrice(), thongKe.getDate(),
+                            thongKe.getTotalAmount());
+                    System.out.println(read);
+                }
+                System.out.format(
+                        "+----------+--------------+------------+--------------+-----------------+-----------------+%n");
+                System.out.println("Khách hàng mua nhiều nhất:");
+                System.out.println("ID khách hàng: " + TK[0].getCustomerId());
+                System.out.println("Tổng số lượng mua: " + calculateTotalQuantityForCustomer(TK[0].getCustomerId()));
+            }
             default -> {
                 System.out.println("Lựa chọn không hợp lệ!");
             }
         }
-
-        System.out.println("Danh sách thống kê:");
-        String header = String.format("| %-8s | %-12s | %-10s | %-12s | %-15s | %-15s |", "ID khách", "ID sản phẩm", "Số lượng", "Giá",
-                "Ngày", "Tổng");
-        System.out.format("+----------+--------------+------------+--------------+-----------------+-----------------+%n");
-        System.out.println(header);
-        System.out.format("+----------+--------------+------------+--------------+-----------------+-----------------+%n");
-        for (ThongKe thongKe : result) {
-            String read = String.format("| %-8s | %-12s | %-10s | %-12s | %-15s | %-15s |",thongKe.getCustomerId(), thongKe.getProductId(),
-                    thongKe.getAmount(), thongKe.getPrice(), thongKe.getDate(), thongKe.getTotalAmount());
-            System.out.println(read);
-        }
-        System.out.format("+----------+--------------+------------+--------------+-----------------+-----------------+%n");
-        System.out.println("Tổng số lượng đã bán ra: " + totalAmount);
-        System.out.println("Doanh thu: " + totalPrice);
         waitConsole();
     }
-    
+
     private LocalDate inputDate(String i) {
         LocalDate date = null;
         do {
